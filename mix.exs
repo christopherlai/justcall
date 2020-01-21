fd5e5bbc3715d70e7a55eaf62ba775a03466d3ad
Christopher Lai <christopherliamlai@me.com>
2020-01-21 10:08:13 -0800

    Prepare mix.exs for hex publish

--- mix.exs
+++ mix.exs
@@ -1,12 +1,18 @@
 defmodule Justcall.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @url "https://github.com/christopherlai/justcall"
+  @name "Justcall"
+
   def project do
     [
       app: :justcall,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      docs: docs(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -21,6 +27,24 @@ defmodule Justcall.MixProject do
       {:ex_doc, "~> 0.21.2", only: :dev, runtime: false},
       {:hackney, "~> 1.15", optional: true},
       {:jason, "~> 1.1", optional: true}
+    ]
+  end
+
+  defp docs do
+    [
+      main: @name,
+      source_ref: "v#{@version}",
+      source_url: @url,
+      extras: ["README.md"]
+    ]
+  end
+
+  defp package() do
+    [
+      name: @name,
+      files: ~w(lib config .formatter.exs mix.exs README*),
+      licenses: ["MIT"],
+      links: %{github: @url}
     ]
   end
 end
